84b6a914a7aeced5405c21b02e5e1362dff0431a
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2023-04-30 14:55:28 +0100

    modified the eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,26 @@
 module.exports = {
-  // parser: '@typescript-eslint/parser',
   env: {
     es2021: true,
     node: true
   },
   extends: ['plugin:@typescript-eslint/recommended'],
-  overrides: [],
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ['*.ts', '*.mts', '*.cts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'error'
+      }
+    }
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  ignorePatterns: ["**/*.js"],
+  // ignorePatterns: ['**/*.js'],
   rules: {
-    semi: 'error',
+    semi: ['error', 'never'],
     indent: ['error', 2],
-    '@typescript-eslint/explicit-function-return-type': 'error'
+    quotes: ['error', 'single'],
   }
-};
+}
--- packages/user-service/src/index.ts
+++ packages/user-service/src/index.ts
@@ -1,5 +1,14 @@
-function sayHello(name: string): void {
-  console.log('Hello World' + name);
-}
+import express, { type Request, Response } from 'express'
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env.dev' })
 
-sayHello("John");
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'user service is running' })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
