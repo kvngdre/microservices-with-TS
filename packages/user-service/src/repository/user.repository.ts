62e58fcaf72909674aa7fcf963294fbc96ea40e5
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2023-05-01 13:42:52 +0100

    fixed bug with user model dependency injection

--- packages/user-service/src/repository/user.repository.ts
+++ packages/user-service/src/repository/user.repository.ts
@@ -1,12 +1,18 @@
-import { Service } from 'typedi'
+import Container, { Service } from 'typedi'
 
 import User from '../model/user.model'
-import IUserRequest from '../dto/userRequest.dto'
+import type IUserRequest from '../dto/userRequest.dto'
+import type IUserModel from '../interfaces/userModel.interface'
 import { type IUserDoc } from '../interfaces/userModel.interface'
 
+Container.set('userModel', User)
 @Service()
 class UserRepository {
-  constructor(private readonly userModel = User) {}
+  private readonly userModel: IUserModel
+
+  constructor() {
+    this.userModel = Container.get('userModel')
+  }
 
   public async addUser(newUserDto: IUserRequest): Promise<IUserDoc> {
     const newUser = new this.userModel(newUserDto)
